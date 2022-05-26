c0414b8098a04c49c223ccbd2652a815d5b998d9
kanika637 <kanikagola26@gmail.com>
2022-05-26 23:39:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const path=require('path')  
+
+const app=express()
+
+app.use('/', express.static(path.join(__dirname,'static')))
+
+//for registering
+app.post(' /api/auth/register',async(req,res)=>{
+    console.log(req.body)
+})
+
+app.listen(5000, ()=>{
+    console.log('Server up at 5000')
+})
